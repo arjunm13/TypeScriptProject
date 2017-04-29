3ef7e02e70902be939e608d42d38f79785cd2cad
arjunm13 <arjunmahen13@gmail.com>
2017-04-29 15:48:46 -0400

    Working Typescript and Reactjs base

---
+++ src/components/Hello.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface HelloProps { compiler: string; framework: string; }
+
+export class Hello extends React.Component<HelloProps, undefined>{
+    render(){
+        console.log(this.props.compiler);
+        return <h1> Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    }
+
+}
