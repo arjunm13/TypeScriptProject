e703fb6c5509a308d21aab89d36a6f750e0d42ea
arjunm13 <arjunmahen13@gmail.com>
2017-04-29 14:11:06 -0400

    helllo greeting working with TS objects

---
+++ greeter.ts
@@ -0,0 +1,25 @@
+class Student{
+    
+    fullName : string; 
+    constructor(public firstName, public middleInitial, public lastName){
+
+        this.fullName = firstName+' '+ middleInitial+' '+ lastName;  
+
+    }
+
+}
+interface Person{
+    firstName : string;
+    lastName : string;
+
+}
+function greeter(person: Person){
+
+    return "hello : " + person.firstName + ' '+person.lastName;
+
+}
+
+var user = new Student("Arjun", "G.", "Mahendran");
+
+document.body.innerHTML = greeter(user);
+
